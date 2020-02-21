9c6b33b6d23a2dd97a502fc45537d95605633115
kengne ruphine <ruphinekengne@gmail.com>
2020-02-21 11:14:07 +0100

    Updates

---
+++ precache-manifest.ba2b039807b5a5295bc39261b5e637ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092f4c2b38c2c6e7dedb464cc7af38e5",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "de4453ea9a7153c69db5",
+    "url": "/react-tutorial/static/css/main.71bd5e9e.chunk.css"
+  },
+  {
+    "revision": "e257cc4ac4564e43f115",
+    "url": "/react-tutorial/static/js/2.29478003.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tutorial/static/js/2.29478003.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4453ea9a7153c69db5",
+    "url": "/react-tutorial/static/js/main.c297ae16.chunk.js"
+  },
+  {
+    "revision": "d1a4ad59d776d93e8cfb",
+    "url": "/react-tutorial/static/js/runtime-main.4b063f4c.js"
+  }
+]);
